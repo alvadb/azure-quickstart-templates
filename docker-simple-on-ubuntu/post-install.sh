17a1129e2498fc78dff73fbba55b74077db7c3e6
alvadb <alvab@outlook.com>
2016-04-19 13:22:06 -0700

    fix bugs

--- docker-simple-on-ubuntu/post-install.sh
+++ docker-simple-on-ubuntu/post-install.sh
@@ -8,11 +8,14 @@ echo "dockerName=\${1//.}" >> buildDockerWAR.sh
 echo "dockerName=\`echo \"\$dockerName\" | tr '[:upper:]' '[:lower:]'\`" >> buildDockerWAR.sh
 echo "docker rm  \$(docker stop \$(docker ps -a -q --filter ancestor=easydeploy/\$dockerName --format=\"{{.ID}}\"))" >> buildDockerWAR.sh
 echo "mkdir \$dockerName" >> buildDockerWAR.sh
-echo "rm \$1" >> buildDockerWAR.sh
+echo "cd \$dockerName" >> buildDockerWAR.sh
 echo "cp ../\$1 ." >> buildDockerWAR.sh
+echo "rm ../\$1" >> buildDockerWAR.sh
 echo "rm Dockerfile" >> buildDockerWAR.sh
 echo "touch Dockerfile" >> buildDockerWAR.sh
 echo "echo \"FROM tomcat:8.0.20-jre8\" >> Dockerfile" >> buildDockerWAR.sh
-echo "echo \"COPY /$1 /usr/local/tomcat/webapps/\$1\" >> Dockerfile" >> buildDockerWAR.sh
+echo "echo \"COPY /\$1 /usr/local/tomcat/webapps/\$1\" >> Dockerfile" >> buildDockerWAR.sh
 echo "docker build -t easydeploy/\$dockerName ." >> buildDockerWAR.sh
 echo "docker run -it -d -p \$2:8080 easydeploy/\$dockerName" >> buildDockerWAR.sh
+
+chmod +x buildDockerWAR.sh 
